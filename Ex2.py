c58a3b21d4cb8c890e4b569f2c62d67836a01c30
PK14effect <101181248+PK14effect@users.noreply.github.com>
2022-03-19 19:46:06 +0700

    Add files via upload

---
+++ Ex2.py
@@ -0,0 +1,28 @@
+Mydict = {
+    "color" : "red",
+    "fruit" : "mango",
+    "year"  : "2022" 
+}
+x = Mydict.items()
+print(Mydict)
+print(x)
+
+for i in Mydict.keys():
+    print(i)
+
+for a in Mydict:
+    print(Mydict[a])
+
+
+Mydict["animal"] = ("cat","dog")
+print(Mydict)
+print(Mydict.get("color"))
+
+Myset = {
+(Mydict.get("color")),
+(Mydict.get("fruit")),
+(Mydict.get("year")),
+(Mydict.get("animal"))
+}
+print(Myset)
+
